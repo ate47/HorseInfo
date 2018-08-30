9b026656e89af6a90a0efcee9968b0641716fb54
Antoine Willerval <wilate@yahoo.fr>
2018-08-30 15:00:59 +0200

    1.13 update

---
+++ src/main/java/fr/atesab/horsedebug/BuildAPI.java
@@ -0,0 +1,13 @@
+package fr.atesab.horsedebug;
+
+
+/**
+ * A class to define what an API need to run HorseDebug
+ */
+public interface BuildAPI {
+
+	/**
+	 * the API name (Rift, Forge, (...))
+	 */
+	public String getAPIName();
+}
